a28c46d12806009d78e7af11c6097b4ee40dde2a
Krystan Menne <kmenne@gmail.com>
2016-05-28 22:33:11 -0700

    add method to set entries

---
+++ react-redux-voting/voting-server/test/core_spec.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { List, Map } from 'immutable';
+
+import { setEntries } from '../src/core';
+
+describe('application logic', () => {
+  describe('setEntries', () => {
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: entries
+      }));
+    });
+  });
+});
