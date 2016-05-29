e232fa673b7f422d3039f1e9c928acb7d2ad8d89
Krystan Menne <kmenne@gmail.com>
2016-05-29 14:28:40 -0700

    subscribe a listener to the store that emits it as a state event on the server

--- react-redux-voting/voting-server/index.js
+++ react-redux-voting/voting-server/index.js
@@ -1,5 +1,5 @@
 import makeStore from './src/store';
-import startServer from './src/server';
+import { startServer } from './src/server';
 
 export const store = makeStore();
-startServer();
+startServer(store);
--- react-redux-voting/voting-server/src/server.js
+++ react-redux-voting/voting-server/src/server.js
@@ -1,5 +1,7 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(() => io.emit('state', store.getState().toJS()));
 }
