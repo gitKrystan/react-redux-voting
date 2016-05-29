5d9cc25ba76ff59951fb365377e785bd10d75aa6
Krystan Menne <kmenne@gmail.com>
2016-05-29 14:31:36 -0700

    emit current state upon connection event

--- react-redux-voting/voting-server/src/server.js
+++ react-redux-voting/voting-server/src/server.js
@@ -3,5 +3,11 @@ import Server from 'socket.io';
 export default function startServer(store) {
   const io = new Server().attach(8090);
 
+  // subscribe to state
   store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+  // emit current state upon connection event
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
 }
