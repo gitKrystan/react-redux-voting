32adb67e918c743e2141957a8a18da963cc0d132
Krystan Menne <kmenne@gmail.com>
2016-05-30 16:06:17 -0700

    add callback to voting buttons

--- react-redux-voting/voting-client/src/components/Voting.jsx
+++ react-redux-voting/voting-client/src/components/Voting.jsx
@@ -7,7 +7,8 @@ export default React.createClass({
   render: function() {
     return <div className="voting">
       {this.getPair().map(entry =>
-        <button key={entry}>
+        <button key={entry}
+                onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
         </button>
       )}
--- react-redux-voting/voting-client/test/components/Voting_spec.jsx
+++ react-redux-voting/voting-client/test/components/Voting_spec.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithTag
+  scryRenderedDOMComponentsWithTag,
+  Simulate
 } from 'react-addons-test-utils';
-import { expect } from 'chai';
 import Voting from '../../src/components/Voting';
+import { expect } from 'chai';
 
 describe('Voting', () => {
   it('renders a pair of buttons', () => {
@@ -17,5 +18,18 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 });
